c65dbe724879814516c20b0c0a0b789548e66cae
Javier Chames <haimch@gmail.com>
2019-10-14 22:50:00 +0300

    Added improments
    improvments of functions
    added readme
    removed SQL files and put in hte code the query

--- checkfiles.py
+++ checkfiles.py
@@ -5,84 +5,52 @@ import mysql.connector
 
 credentials_file_name = "credentials.txt"
 path_file_name = "paths.txt"
-query_file_name = "sql_query.sql"
-update_db_query_file = "update_sql_query.sql"
 host_to_ping = "host_to_ping.txt"
 
 
-def read_from_paths(file_name):
-    with open(file_name) as f:
-        lineList = f.read().splitlines()
-    f.close
-    return lineList
-
-
 def read_from_file(file_name):
-    with open(file_name) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
-    f.close
-    return content
+    with open(file_name,'r') as f:
+        file_content = f.read().splitlines()
+    return file_content
 
-
-def print_last_modified_time():
+def get_modified_time():
     time_stamps = []
     # call to function to read paths from file
-    for path in read_from_paths(path_file_name):
+    for path in read_from_file(path_file_name):
         modified_date_time_before_format = time.ctime(os.path.getmtime(path))
-        time_stamps.append(str(datetime.datetime.strptime(
-            modified_date_time_before_format, "%a %b %d %H:%M:%S %Y")))
+        time_stamps.append(datetime.datetime.strptime(
+            modified_date_time_before_format, "%a %b %d %H:%M:%S %Y"))
     return time_stamps
-
-
-def read_one_line_from_file(file_name):
-    f = open(file_name)
-    query = f.readline()
-    f.close
-    return query
-
 
 def read_from_db(credentials):
 
-    query = read_one_line_from_file(query_file_name)
+    query = "select * from Files"
     db_credentials = []
-    for each_credential in credentials:
-        db_credentials.append(each_credential)
-
     mydb = mysql.connector.connect(
-        host=db_credentials[0],
-        user=db_credentials[1],
-        passwd=db_credentials[2],
-        database=db_credentials[3]
+    host=credentials[0],
+    user=credentials[1],
+    passwd=credentials[2],
+    database=credentials[3]
     )
-
     mycursor = mydb.cursor()
     mycursor.execute(query)
-
     field_names = [i[0] for i in mycursor.description]
     return mycursor.fetchone(), field_names, mycursor, mydb
 
-
 def check_needed_update(sql_result, files_time_stamps, field_names, mycursor, mydb):
-    update_query = read_one_line_from_file(update_db_query_file)
     flag = 0
-    sql = list(sql_result)
-    for x in range(len(sql)):
-        if str(sql[x]) != str(files_time_stamps[x]):
+    sql_result = list(sql_result)
+    for x in range(len(sql_result)):
+        if str(sql_result[x]) != str(files_time_stamps[x]):
             flag += 1
-            update_query_sql = update_query + \
-                str(field_names[x]) + " =" + \
-                '"{}"'.format(str(files_time_stamps[x]))
+            update_query_sql = "select * from Files" + field_names[x] + " =" + '"{}"'.format((files_time_stamps[x]))
             print(update_query_sql)
             mycursor.execute(update_query_sql)
             mydb.commit()
     return flag
 
-
 def ping_ip():
-    if os.system("ping -c 1 " + read_one_line_from_file(host_to_ping)) == 0:
-        return True
-
+    return os.system("ping -c 1 " + ''.join(read_from_file(host_to_ping))) == 0
 
 def run_docker():
     myCmd = 'docker start kodi-docker'
@@ -91,9 +59,10 @@ def run_docker():
     myCmd = 'docker stop kodi-docker'
     os.system(myCmd)
 
-if ping_ip() == True:
-    credentials = read_from_file(credentials_file_name)
-    files_time_stamps = print_last_modified_time()
-    sql_query_result, field_names, mycursor, mydb = read_from_db(credentials)
-    if check_needed_update(sql_query_result, files_time_stamps, field_names, mycursor, mydb):
-        run_docker()
+if __name__ == '__main__':
+    if ping_ip() == True:
+        credentials = read_from_file(credentials_file_name)
+        files_time_stamps = get_modified_time()
+        sql_query_result, field_names, mycursor, mydb = read_from_db(credentials)
+        if check_needed_update(sql_query_result, files_time_stamps, field_names, mycursor, mydb):
+            run_docker()
