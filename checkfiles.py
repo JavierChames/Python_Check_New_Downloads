964b5ffed5bf7c01d9a8bf30af281271cd658eb9
Javier Chames <haimch@gmail.com>
2019-10-19 17:26:27 +0300

    Some mroe fixes in functions
    fixes of  indentation

--- checkfiles.py
+++ checkfiles.py
@@ -9,9 +9,10 @@ host_to_ping = "host_to_ping.txt"
 
 
 def read_from_file(file_name):
-    with open(file_name,'r') as f:
+    with open(file_name, 'r') as f:
         file_content = f.read().splitlines()
     return file_content
+
 
 def get_modified_time():
     time_stamps = []
@@ -22,20 +23,20 @@ def get_modified_time():
             modified_date_time_before_format, "%a %b %d %H:%M:%S %Y"))
     return time_stamps
 
-def read_from_db(credentials):
 
+def read_from_db(credentials):
     query = "SELECT * FROM Files"
-    db_credentials = []
     mydb = mysql.connector.connect(
-    host=credentials[0],
-    user=credentials[1],
-    passwd=credentials[2],
-    database=credentials[3]
+        host=credentials[0],
+        user=credentials[1],
+        passwd=credentials[2],
+        database=credentials[3]
     )
     mycursor = mydb.cursor()
     mycursor.execute(query)
     field_names = [i[0] for i in mycursor.description]
     return mycursor.fetchone(), field_names, mycursor, mydb
+
 
 def check_needed_update(sql_result, files_time_stamps, field_names, mycursor, mydb):
     flag = 0
@@ -43,27 +44,32 @@ def check_needed_update(sql_result, files_time_stamps, field_names, mycursor, my
     for x in range(len(sql_result)):
         if str(sql_result[x]) != str(files_time_stamps[x]):
             flag += 1
-            update_query_sql = "UPDATE Files SET " + field_names[x] + " =" + '"{}"'.format((files_time_stamps[x]))
+            update_query_sql = "UPDATE Files SET " + \
+                field_names[x] + " =" + '"{}"'.format((files_time_stamps[x]))
             mycursor.execute(update_query_sql)
             mydb.commit()
             mycursor.close()
             mydb.close()
     return flag
 
+
 def ping_ip():
     return os.system("ping -c 1 " + ''.join(read_from_file(host_to_ping))) == 0
 
+
 def run_docker():
-    myCmd = 'docker start kodi-docker'
-    os.system(myCmd)
+    mycmd = 'docker start kodi-docker'
+    os.system(mycmd)
     time.sleep(60)
-    myCmd = 'docker stop kodi-docker'
-    os.system(myCmd)
+    mycmd = 'docker stop kodi-docker'
+    os.system(mycmd)
+
 
 if __name__ == '__main__':
     if ping_ip() == True:
         credentials = read_from_file(credentials_file_name)
         files_time_stamps = get_modified_time()
-        sql_query_result, field_names, mycursor, mydb = read_from_db(credentials)
+        sql_query_result, field_names, mycursor, mydb = read_from_db(
+            credentials)
         if check_needed_update(sql_query_result, files_time_stamps, field_names, mycursor, mydb):
             run_docker()
